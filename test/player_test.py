32d6c5708aa0dfbf4b5219d5c121a5e21f506770
cristi <mastercris78@yahoo.com>
2023-12-05 16:44:48 +0200

    Test files added

--- test/player_test.py
+++ test/player_test.py
@@ -2,61 +2,39 @@ import pytest
 
 from player import Player
 
-test_player = Player()
 
+class TestPlayer:
 
-def test_start_pos():
+    def setup_method(self):
+        self.test_player = Player()
 
-    assert test_player.xcor() == 0 and test_player.ycor() == -280
+    def test_player_start_pos(self):
+        assert self.test_player.xcor() == 0 and self.test_player.ycor() == -280, "Player did not start in the specified starting position"
 
+    def test_player_move_up(self):
+        current_pos = self.test_player.ycor()
+        self.test_player.turtle_move_up()
+        new_pos = self.test_player.ycor()
+        assert new_pos - current_pos == 10, "Player did not move 10 units up"
 
-def test_player_move_up():
-    assert test_player.turtle_move_up() == test_player.forward(10)
+    def test_player_move_down(self):
+        current_pos = self.test_player.ycor()
+        self.test_player.turtle_move_down()
+        new_pos = self.test_player.ycor()
+        assert new_pos - current_pos == -10, "Player did not move 10 units down"
 
+    def test_player_reset_pos(self):
+        assert self.test_player.player_reset() == -280, "Player did not go to the specified starting position"
 
-def test_player_move_down():
-    assert test_player.turtle_move_down() == test_player.backward(10)
+    def test_player_shape(self):
+        assert self.test_player.shape() == "turtle", "Player shape is incorrect"
 
+    def test_player_color(self):
+        assert self.test_player.color() == ('green', 'green'), "Player color is incorrect"
 
-def test_player_reset_pos():
-    assert test_player.player_reset() == test_player.goto(0, -280)
+    def test_player_heading(self):
+        assert self.test_player.heading() == 90, "Player heading is incorrect"
 
+    def test_player_penup(self):
+        assert self.test_player.isdown() == False, "Turtle pen currently on"
 
-def test_player_color():
-    assert test_player.color() == ('green', 'green')
-
-
-def test_player_shape():
-    assert test_player.shape() == "turtle"
-
-
-def test_player_heading():
-    assert test_player.heading() == 90
-
-
-# class PlayerTest:
-#
-#     def test_player(self):
-#         self.test_player = Player()
-#
-#     def test_player_start_pos(self):
-#         assert self.test_player.xcor() == 0
-#         assert self.test_player.ycor() == -280
-#
-#     def test_player_move_up(self):
-#         assert self.test_player.turtle_move_up() == self.test_player.forward(10)
-#
-#     def test_player_move_down(self):
-#         assert self.test_player.turtle_move_down() == self.test_player.backward(10)
-#
-#     def test_player_reset_pos(self):
-#         assert self.test_player.player_reset() == self.test_player.goto(0, -280)
-#
-#     def test_player_shape(self):
-#         assert self.test_player.shape() == "turtle"
-#
-#     def test_player_color(self):
-#         assert self.test_player.color() == ('green', 'green')
-#
-#     def test_player_heading(self):
-#         assert self.test_player.heading() == 90
